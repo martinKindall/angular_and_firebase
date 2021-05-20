f18728416bf157205edfc7938658fd32ea415345
martinKindall <mart256@gmail.com>
2021-05-20 22:19:10 +0200

    Deleted default page. Added firebase auth ui and some callbacks.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FirebaseUISignInFailure, FirebaseUISignInSuccessWithAuthResult} from 'firebaseui-angular';
+import {AngularFireAuth} from '@angular/fire/auth';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'firebase-realtime-db';
+export class AppComponent implements OnInit {
+  constructor(private angularFireAuth: AngularFireAuth) {
+  }
+
+  ngOnInit(): void {
+    this.angularFireAuth.authState.subscribe(this.firebaseAuthChangeListener);
+  }
+
+  successCallback(signInSuccessData: FirebaseUISignInSuccessWithAuthResult): void {
+    console.log('Hallo');
+  }
+
+  errorCallback(errorData: FirebaseUISignInFailure): void {
+    console.log('Einloggen war nicht erfolgreich.');
+  }
+
+  uiShownCallback(): void {
+    console.log('Ui shown');
+  }
+
+  private firebaseAuthChangeListener(response): void {
+    // if needed, do a redirect in here
+    if (response) {
+      console.log('Logged in :)');
+    } else {
+      console.log('Logged out :(');
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,32 @@ import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import {AngularFireModule} from '@angular/fire';
 
+import {firebase, firebaseui, FirebaseUIModule} from 'firebaseui-angular';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+
+const firebaseUiAuthConfig: firebaseui.auth.Config = {
+  signInFlow: 'popup',
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    {
+      requireDisplayName: false,
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID
+    }
+  ],
+  tosUrl: 'www.google.com',
+  privacyPolicyUrl: 'www.google.com',
+  credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase)
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    FirebaseUIModule.forRoot(firebaseUiAuthConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
